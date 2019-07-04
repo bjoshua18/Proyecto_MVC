e59e0482372383792c41ccfe724d7859fc145a79
Byron <byronbl.12@gmail.com>
2019-07-04 20:57:46 +0100

    Logica del Modelo y Controlador de las vistas Modifico algunas cosas que creo que son redundantes Aún no se ha comprobado que funcione

--- modelos/vistasModelo.php
+++ modelos/vistasModelo.php
@@ -1,5 +1,20 @@
 <?php
 
 class vistasModelo {
-	
+
+	protected function obtener_vistas_modelo($vistas) {
+		$admitidos = ['adminlist', 'adminsearch', 'admin', 'book', 'bookconfig', 'bookinfo', 'catalog', 'category', 'categorylist', 'client', 'clientlist', 'clientsearch', 'company', 'companylist', 'home', 'myaccount', 'mydata', 'provider', 'providerlist', 'search'];
+
+		if(in_array($vistas, $admitidos)) {
+			$ruta = "./vistas/contenidos/$vistas-view.php";
+
+			if(is_file($ruta))
+				$contenido = $ruta;
+			else
+				$contenido = 'login';
+				
+		} else {
+			$contenido = 'login';
+		}
+	}
 }
