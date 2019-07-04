02c54d2e547b8b334e72e018776dcdaafb4936a1
Byron <byronbl.12@gmail.com>
2019-07-04 20:19:24 +0100

    Creando el Controlador para obtener plantilla Creamos un método en vistasControlador para obtener la vista en index

--- controladores/vistasControlador.php
+++ controladores/vistasControlador.php
@@ -3,4 +3,7 @@ require_once('./modelos/vistasModelo.php');
 
 class vistasControlador extends vistasModelo {
 	
+	public function obtener_plantilla_controlador() {
+		require_once('./vistas/plantilla.php');
+	}
 }
