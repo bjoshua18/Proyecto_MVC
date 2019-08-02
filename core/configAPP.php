88486cfe790e672df3bc44156eb8a17d47d56d18
Byron Bustamante <byronbl.12@gmail.com>
2019-08-02 15:57:55 +0100

    Funciones para encriptar y desencriptar strings

--- core/configAPP.php
+++ core/configAPP.php
@@ -1,8 +1,16 @@
 <?php
 
+// DATABASE
+
 const SERVER = "";
 const DB = "";
 const USER = "";
 const PASS = "";
 
 const SGBD = 'mysql:host='.SERVER.';dbname='.DB;
+
+// ENCRIPTACION. No cambiar si ya hemos usado la encriptacion
+
+const METHOD = 'AES-256-CBC';
+const SECRET_KEY = '$proyecto@mvc@2019$';
+const SECRET_IV = '02082019';
--- core/mainModel.php
+++ core/mainModel.php
@@ -12,4 +12,20 @@ class mainModel {
 		$respuesta->execute();
 		return $respuesta;
 	}
+
+	public function encryption($string) {
+		$output = false;
+		$key = hash('sha256', SECRET_KEY);
+		$iv = substr(hash('sha256', SECRET_IV), 0, 16);
+		$output = openssl_encrypt($string, METHOD, $key, 0, $iv);
+		$output = base64_encode($output);
+		return $output;
+	}
+
+	protected function decryption($string) {
+		$key = hash('sha256', SECRET_KEY);
+		$iv = substr(hash('sha256', SECRET_IV), 0, 16);
+		$output = openssl_decrypt(base64_decode($string), METHOD, $key, 0, $iv);
+		return $output;
+	}
 }
