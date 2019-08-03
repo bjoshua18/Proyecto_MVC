3e4ec5b4ecc95abdd851b35d91ec5fd1e73ce3b7
Byron Bustamante <byronbl.12@gmail.com>
2019-08-03 22:02:00 +0100

    Modelo agregar administrador

--- controladores/administradorControlador.php
+++ controladores/administradorControlador.php
@@ -3,5 +3,7 @@
 $peticionAjax ? require_once "../modelos/administradorModelo" : require_once "./modelos/administradorModelo";
 
 class administradorControlador extends administradorModelo {
-	
+	public function agregar_administrador_controlador() {
+
+	}
 }
