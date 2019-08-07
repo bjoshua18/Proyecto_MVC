7ec0e3e19437d7c499e72c1b67613a6942eb778a
Byron Bustamante <byronbl.12@gmail.com>
2019-08-07 13:25:17 +0100

    Controlador para inciar sesion (2-3)

--- controladores/loginControlador.php
+++ controladores/loginControlador.php
@@ -17,7 +17,41 @@ class loginControlador extends loginModelo {
 		$datosCuenta = loginModelo::inciar_sesion_modelo($datosLogin);
 
 		if($datosCuenta->rowCount() == 1) {
-			
+			$row = $datosCuenta->fetch();
+
+			$fechaActual = date("Y-m-d");
+			$yearActual = date('Y');
+			$horaActual = date('h:i:s a');
+
+			$consulta1 = mainModel::ejecutar_consulta_simple("SELECT id FROM bitacora");
+
+			$numero = ($consulta1->rowCount())+1;
+
+			$codigoB = mainModel::generar_codigo_aleatorio("CB", 7, $numero);
+
+			$datosBitacora = [
+				'Codigo' => $codigoB,
+				'Fecha' => $fechaActual,
+				'HoraInicio' => $horaActual,
+				'HoraFinal' => "Sin registro",
+				'Tipo' => $row['CuentaTipo'],
+				'Year' => $yearActual,
+				'Cuenta' => $row['CuentaCodigo']
+			];
+
+			$insertarBitacora = mainModel::guardar_bitacora($datosBitacora);
+			if($insertarBitacora->rowCount() >= 1) {
+				
+			} else {
+				$alerta = [
+					'Alerta' => 'simple',
+					'Titulo' => 'Ocurrió un error inesperado',
+					'Texto' => 'No hemos podido iniciar la sesión por problemas técnicos, por favor, intente nuevamente',
+					'Tipo' => 'error'
+				];
+	
+				return mainModel::sweet_alert($alerta);
+			}
 		} else {
 			$alerta = [
 				'Alerta' => 'simple',
