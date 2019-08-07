bfa72d2f6e3820706128e3d6198ea97f794fcd7d
Byron Bustamante <byronbl.12@gmail.com>
2019-08-07 13:06:36 +0100

    Controlador para inciar sesion (1-3)

--- controladores/loginControlador.php
+++ controladores/loginControlador.php
@@ -4,6 +4,29 @@ $peticionAjax ? require_once "../modelos/loginModelo.php" : require_once "./mode
 class loginControlador extends loginModelo {
 	
 	public function inciar_sesion_controlador() {
-		
+		$usuario = mainModel::limpiar_cadena($_POST['usuario']);
+		$clave = mainModel::limpiar_cadena($_POST['clave']);
+
+		$clave = mainModel::encryption($clave);
+
+		$datosLogin = [
+			'Usuario' => $usuario,
+			'Clave' => $clave
+		];
+
+		$datosCuenta = loginModelo::inciar_sesion_modelo($datosLogin);
+
+		if($datosCuenta->rowCount() == 1) {
+			
+		} else {
+			$alerta = [
+				'Alerta' => 'simple',
+				'Titulo' => 'Ocurrió un error inesperado',
+				'Texto' => 'El nombre de usuario o contraseña no son correctos o su cuenta puede estar deshabilitada',
+				'Tipo' => 'error'
+			];
+
+			return mainModel::sweet_alert($alerta);
+		}
 	}
 }
