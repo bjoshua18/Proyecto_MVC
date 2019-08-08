25e6c32f9e2f16aa0b6714fe260c3a2f62ffdf9c
Byron Bustamante <byronbl.12@gmail.com>
2019-08-08 08:55:15 +0100

    Programando logoutScript

--- vistas/modulos/logoutScript.php
+++ vistas/modulos/logoutScript.php
@@ -2,17 +2,31 @@
 	$(document).ready(function() {
 		$('.btn-exit-system').on('click', function(e){
 			e.preventDefault();
+			var Token = $(this).attr('href');
 			swal({
-					title: 'Are you sure?',
-					text: "The current session will be closed",
+					title: '¿Estás seguro?',
+					text: "La sesión actual se cerrará y deberás cerrar sesion nuevamente",
 					type: 'warning',
 					showCancelButton: true,
 					confirmButtonColor: '#03A9F4',
 					cancelButtonColor: '#F44336',
-					confirmButtonText: '<i class="zmdi zmdi-run"></i> Yes, Exit!',
-					cancelButtonText: '<i class="zmdi zmdi-close-circle"></i> No, Cancel!'
+					confirmButtonText: '<i class="zmdi zmdi-run"></i> Sí, cerrar!',
+					cancelButtonText: '<i class="zmdi zmdi-close-circle"></i> No, cancelar!'
 			}).then(function () {
-				window.location.href="index.html";
+				$.ajax({
+					url: "<?= SERVERURL ?>ajax/loginAjax.php?Token="+Token,
+					success: function(data) {
+						if(data == "true") {
+							window.location.href="<?= SERVERURL ?>login/"
+						}else {
+							swal(
+								'Ocurrió un error',
+								'No se pudo cerrar la sesion',
+								'error'
+							)
+						}
+					}
+				})
 			});
 		});
 	})
